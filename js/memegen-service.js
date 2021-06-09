f20465da87612d4ead777e44397114dae6982945
Ori Zaken <zaken1ori@gmail.com>
2021-06-09 15:35:03 +0300

    test

--- js/memegen-service.js
+++ js/memegen-service.js
@@ -3,81 +3,79 @@ var gKeywords = {
     'funny puk': 1
 }
 
-var gImgs = [
-    {
-        id: 1,
-        url: '../img/1.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 2,
-        url: '../img/2.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 3,
-        url: '../img/3.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 4,
-        url: '../img/4.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 5,
-        url: '../img/5.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 6,
-        url: '../img/6.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 7,
-        url: '../img/7.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 8,
-        url: '../img/8.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 9,
-        url: '../img/9.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 10,
-        url: '../img/10.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 11,
-        url: '../img/11.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 12,
-        url: '../img/12.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 13,
-        url: '../img/13.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 14,
-        url: '../img/14.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 15,
-        url: '../img/15.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 16,
-        url: '../img/16.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 17,
-        url: '../img/17.jpg',
-        // keywords: ['happy']
-    }, {
-        id: 19,
-        url: '../img/18.jpg',
-        // keywords: ['happy']
-    }
-]
+var gImgs = [{
+    id: 1,
+    url: '../img/1.jpg',
+    // keywords: ['happy']
+}, {
+    id: 2,
+    url: '../img/2.jpg',
+    // keywords: ['happy']
+}, {
+    id: 3,
+    url: '../img/3.jpg',
+    // keywords: ['happy']
+}, {
+    id: 4,
+    url: '../img/4.jpg',
+    // keywords: ['happy']
+}, {
+    id: 5,
+    url: '../img/5.jpg',
+    // keywords: ['happy']
+}, {
+    id: 6,
+    url: '../img/6.jpg',
+    // keywords: ['happy']
+}, {
+    id: 7,
+    url: '../img/7.jpg',
+    // keywords: ['happy']
+}, {
+    id: 8,
+    url: '../img/8.jpg',
+    // keywords: ['happy']
+}, {
+    id: 9,
+    url: '../img/9.jpg',
+    // keywords: ['happy']
+}, {
+    id: 10,
+    url: '../img/10.jpg',
+    // keywords: ['happy']
+}, {
+    id: 11,
+    url: '../img/11.jpg',
+    // keywords: ['happy']
+}, {
+    id: 12,
+    url: '../img/12.jpg',
+    // keywords: ['happy']
+}, {
+    id: 13,
+    url: '../img/13.jpg',
+    // keywords: ['happy']
+}, {
+    id: 14,
+    url: '../img/14.jpg',
+    // keywords: ['happy']
+}, {
+    id: 15,
+    url: '../img/15.jpg',
+    // keywords: ['happy']
+}, {
+    id: 16,
+    url: '../img/16.jpg',
+    // keywords: ['happy']
+}, {
+    id: 17,
+    url: '../img/17.jpg',
+    // keywords: ['happy']
+}, {
+    id: 19,
+    url: '../img/18.jpg',
+    // keywords: ['happy']
+}]
 
 var gMeme = {
     selectedImgId: 0,
@@ -155,7 +153,6 @@ function changePosition(num) {
 function switchLines() {
     gMeme.selectedLineIdx = (gMeme.selectedLineIdx === 0) ? 1 : 0
 }
-
-    // var selectedImg = gImgs.find(img => img.id === gMeme.selectedImgId)
-    // console.log(selectedImg)
-    // return selectedImg
+// var selectedImg = gImgs.find(img => img.id === gMeme.selectedImgId)
+// console.log(selectedImg)
+// return selectedImg
